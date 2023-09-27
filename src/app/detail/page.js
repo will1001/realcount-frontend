bbab2d905b0f30bf5cc5ef241130516ce50faa51
wiligetsurvey <wili@getsurvey.id>
2023-09-27 16:44:10 +0800

    detail suara pemilih

--- src/app/detail/page.js
+++ src/app/detail/page.js
@@ -1,19 +1,44 @@
 // import { withRouter } from "next/navigation";
 "use client";
+import axiosFetch from "@/API/axiosFetch";
 import useFetch from "@/API/useFetch";
 import { useRouter, useSearchParams } from "next/navigation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [data, setData] = useState(null);
+  const [metaData, setMetaData] = useState(null);
 
-  const pemilihs = useFetch("get", `/pemilih`);
+  const fetchData = async () => {
+    const id_upa = searchParams.get("id_upa");
+    const id_dpr_level = searchParams.get("id_dpr_level");
+    const dapil = searchParams.get("dapil");
+    const id_category = searchParams.get("id_category");
+    const id_sub_category = searchParams.get("id_sub_category");
+    let url = `/pemilih?dapil=${dapil}&id_dpr_level=${id_dpr_level}&${
+      id_upa ? "id_upa=" + id_upa : null
+    }&${id_category ? "id_category=" + id_category : null}&${
+      id_sub_category ? "id_sub_category=" + id_id_sub_categoryupa : null
+    }`;
+
+    try {
+      const res = await axiosFetch("get", url, {}, "token");
+      setData(res.data.data);
+      setMetaData(res.data.metadata);
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1 className="py-5 font-bold">Data Pemilih</h1>
-      {/* {searchParams.get("id_dpr_level")} */}
       <table class="w-full table-auto border text-center">
         <thead>
           <tr>
@@ -26,7 +51,7 @@ const page = () => {
           </tr>
         </thead>
         <tbody>
-          {pemilihs?.data?.map((item, i) => (
+          {data?.map((item, i) => (
             <tr>
               <td class="border">{i + 1}</td>
               <td class="border">{item.nama}</td>
--- src/components/RowTableSuara.js
+++ src/components/RowTableSuara.js
@@ -9,29 +9,80 @@ function RowTableSuara({ data, nomor, index }) {
       <td class="border">dapil {nomor}</td>
       <td
         class="border cursor-pointer underline text-blue-500"
-        onClick={() => router.push(`detail?id_dpr_level=${index}&id_upa=""`)}
+        onClick={() =>
+          router.push(`detail?dapil=${nomor}&id_dpr_level=${index}&id_upa=ada`)
+        }
       >
         {data.data ? data?.data[index]?.anggota.toLocaleString() : 0}
       </td>
-      <td class="border">
+      <td
+        class="border cursor-pointer underline text-blue-500"
+        onClick={() =>
+          router.push(
+            `detail?dapil=${nomor}&id_dpr_level=${index}&id_sub_category=5`
+          )
+        }
+      >
         {data.data ? data?.data[index]?.dpc.toLocaleString() : 0}
       </td>
-      <td class="border">
+      <td
+        class="border cursor-pointer underline text-blue-500"
+        onClick={() =>
+          router.push(
+            `detail?dapil=${nomor}&id_dpr_level=${index}&id_sub_category=6`
+          )
+        }
+      >
         {data.data ? data?.data[index]?.dpra.toLocaleString() : 0}
       </td>
-      <td class="border">
+      <td
+        class="border cursor-pointer underline text-blue-500"
+        onClick={() =>
+          router.push(
+            `detail?dapil=${nomor}&id_dpr_level=${index}&id_category=5`
+          )
+        }
+      >
         {data.data ? data?.data[index]?.bko.toLocaleString() : 0}
       </td>
-      <td class="border">
+      <td
+        class="border cursor-pointer underline text-blue-500"
+        onClick={() =>
+          router.push(
+            `detail?dapil=${nomor}&id_dpr_level=${index}&id_category=4`
+          )
+        }
+      >
         {data.data ? data?.data[index]?.bpkk.toLocaleString() : 0}
       </td>
-      <td class="border">
+      <td
+        class="border cursor-pointer underline text-blue-500"
+        onClick={() =>
+          router.push(
+            `detail?dapil=${nomor}&id_dpr_level=${index}&id_category=7`
+          )
+        }
+      >
         {data.data ? data?.data[index]?.bpu.toLocaleString() : 0}
       </td>
-      <td class="border">
+      <td
+        class="border cursor-pointer underline text-blue-500"
+        onClick={() =>
+          router.push(
+            `detail?dapil=${nomor}&id_dpr_level=${index}&id_category=6`
+          )
+        }
+      >
         {data.data ? data?.data[index]?.kepemudaan.toLocaleString() : 0}
       </td>
-      <td class="border">
+      <td
+        class="border cursor-pointer underline text-blue-500"
+        onClick={() =>
+          router.push(
+            `detail?dapil=${nomor}&id_dpr_level=${index}&id_category=8`
+          )
+        }
+      >
         {data.data ? data?.data[index]?.tn.toLocaleString() : 0}
       </td>
     </tr>
